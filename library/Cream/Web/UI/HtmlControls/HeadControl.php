4fd2d0f556d52c5766d32fa08f5545e66f616075
Danny Verkade <danny@cream.nl>
2011-10-28 09:16:47 +0200

    Added ability to set css and js files in the head.

--- library/Cream/Web/UI/HtmlControls/HeadControl.php
+++ library/Cream/Web/UI/HtmlControls/HeadControl.php
@@ -34,14 +34,222 @@ class Cream_Web_UI_HtmlControls_HeadControl extends Cream_Web_UI_WebControls_Tem
         parent::__init($data);
         $this->setTemplate('html/head.phtml');
     }
-
-    public function addJs()
+    
+    /**
+     * Merge static and skin files of the same format into 1 set of HEAD directives or even into 1 directive
+     *
+     * Will attempt to merge into 1 directive, if merging callback is provided. In this case it will generate
+     * filenames, rather than render urls.
+     * The merger callback is responsible for checking whether files exist, merging them and giving result URL
+     *
+     * @param string $format - HTML element format for sprintf('<element src="%s"%s />', $src, $params)
+     * @param array $staticItems - array of relative names of static items to be grabbed from js/ folder
+     * @param array $skinItems - array of relative names of skin items to be found in skins according to design config
+     * @param callback $mergeCallback
+     * @return string
+     */
+    protected function &_prepareStaticAndSkinElements($format, array $staticItems, array $skinItems, $mergeCallback = null)
     {
-    	
+        $designPackage = $this->getLayout()->getController()->getDesign();
+        //$baseJsUrl = Mage::getBaseUrl('js');
+        $baseJsUrl = '/js/';
+        
+        $items = array();
+        if ($mergeCallback && !is_callable($mergeCallback)) {
+            $mergeCallback = null;
+        }
+
+        // get static files from the js folder, no need in lookups
+        foreach ($staticItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? Mage::getBaseDir() . DS . 'js' . DS . $name : $baseJsUrl . $name;
+            }
+        }
+
+        // lookup each file basing on current theme configuration
+        foreach ($skinItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? $designPackage->getFilename($name, array('_type' => 'skin'))
+                    : $designPackage->getSkinUrl($name, array());
+            }
+        }
+
+        $html = '';
+        foreach ($items as $params => $rows) {
+            // attempt to merge
+            $mergedUrl = false;
+            if ($mergeCallback) {
+                $mergedUrl = call_user_func($mergeCallback, $rows);
+            }
+            // render elements
+            $params = trim($params);
+            $params = $params ? ' ' . $params : '';
+            if ($mergedUrl) {
+                $html .= sprintf($format, $mergedUrl, $params);
+            } else {
+                foreach ($rows as $src) {
+                    $html .= sprintf($format, $src, $params);
+                }
+            }
+        }
+        return $html;
+    }    
+    
+    /**
+     * Classify HTML head item and queue it into "lines" array
+     *
+     * @see self::getCssJsHtml()
+     * @param array &$lines
+     * @param string $itemIf
+     * @param string $itemType
+     * @param string $itemParams
+     * @param string $itemName
+     * @param array $itemThe
+     */
+    protected function _separateOtherHtmlHeadElements(&$lines, $itemIf, $itemType, $itemParams, $itemName, $itemThe)
+    {
+        $params = $itemParams ? ' ' . $itemParams : '';
+        $href   = $itemName;
+        switch ($itemType) {
+            case 'rss':
+                $lines[$itemIf]['other'][] = sprintf('<link href="%s"%s rel="alternate" type="application/rss+xml" />',
+                    $href, $params
+                );
+                break;
+            case 'link_rel':
+                $lines[$itemIf]['other'][] = sprintf('<link%s href="%s" />', $params, $href);
+                break;
+        }
+    }    
+
+    /**
+     * Add CSS file to HEAD entity
+     *
+     * @param string $name
+     * @param string $params
+     * @return Mage_Page_Block_Html_Head
+     */
+    public function addCss($name, $params = "")
+    {
+        $this->addItem('css', $name, $params);
+    }
+
+    /**
+     * Add JavaScript file to HEAD entity
+     *
+     * @param string $name
+     * @param string $params
+     * @return Mage_Page_Block_Html_Head
+     */
+    public function addJs($name, $params = "")
+    {
+        $this->addItem('js', $name, $params);
     }
     
-    public function addCss()
+    /**
+     * Add HEAD Item
+     *
+     * Allowed types:
+     *  - js
+     *  - js_css
+     *  - skin_js
+     *  - skin_css
+     *  - rss
+     *
+     * @param string $type
+     * @param string $name
+     * @param string $params
+     * @param string $if
+     * @param string $cond
+     * @return Mage_Page_Block_Html_Head
+     */
+    public function addItem($type, $name, $params=null, $if=null, $cond=null)
     {
-    	
+        if ($type==='css' && empty($params)) {
+            $params = 'media="all"';
+        }
+        $this->_data['items'][$type.'/'.$name] = array(
+            'type'   => $type,
+            'name'   => $name,
+            'params' => $params,
+            'if'     => $if,
+            'cond'   => $cond,
+       );
+        return $this;
     }
+
+    /**
+     * Remove Item from HEAD entity
+     *
+     * @param string $type
+     * @param string $name
+     * @return Mage_Page_Block_Html_Head
+     */
+    public function removeItem($type, $name)
+    {
+        unset($this->_data['items'][$type.'/'.$name]);
+        return $this;
+    }    
+    
+    /**
+     * Get HEAD HTML with CSS/JS/RSS definitions
+     * (actually it also renders other elements, TODO: fix it up or rename this method)
+     *
+     * @return string
+     */
+    public function getCssJsHtml()
+    {	    	
+        $lines  = array();
+        foreach ($this->_data['items'] as $item) {
+            if (!is_null($item['cond']) && !$this->getData($item['cond']) || !isset($item['name'])) {
+                continue;
+            }
+            $if     = !empty($item['if']) ? $item['if'] : '';
+            $params = !empty($item['params']) ? $item['params'] : '';
+            
+            switch ($item['type']) {
+                case 'js':        // js/*.js
+                case 'skin_js':   // skin/*/*.js
+                case 'js_css':    // js/*.css
+                case 'css':  // skin/*/*.css
+                    $lines[$if][$item['type']][$params][$item['name']] = $item['name'];
+                    break;
+                default:
+                    $this->_separateOtherHtmlHeadElements($lines, $if, $item['type'], $params, $item['name'], $item);
+                    break;
+            }
+        }
+
+        $html   = '';
+        foreach ($lines as $if => $items) {
+            if (empty($items)) {
+                continue;
+            }
+            if (!empty($if)) {
+                $html .= '<!--[if '.$if.']>'."\n";
+            }
+
+            // static and skin css
+            $html .= $this->_prepareStaticAndSkinElements('<link rel="stylesheet" type="text/css" href="%s"%s />' . "\n",
+                empty($items['js_css']) ? array() : $items['js_css'],
+                empty($items['css']) ? array() : $items['css']
+            );
+
+            // static and skin javascripts
+            $html .= $this->_prepareStaticAndSkinElements('<script type="text/javascript" src="%s"%s></script>' . "\n",
+                empty($items['js']) ? array() : $items['js'],
+                empty($items['skin_js']) ? array() : $items['skin_js']
+            );
+
+            // other stuff
+            if (!empty($items['other'])) {
+                $html .= $this->_prepareOtherHtmlHeadElements($items['other']) . "\n";
+            }
+
+            if (!empty($if)) {
+                $html .= '<![endif]-->'."\n";
+            }
+        }
+        return $html;
+    }    
 }
